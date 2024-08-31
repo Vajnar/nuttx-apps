a97bcbc517733cff9ffd78745b7bd6b1ada1b35d
Martin Vajnar <martin.vajnar@gmail.com>
2024-08-31 15:32:04 +0200

    Modify for PWM control of brushed DC motor
    GPIO 0, 1 - control sense of rotation
    PWM 0 - control speed

--- apo/motor_pwm.c
+++ apo/motor_pwm.c
@@ -20,22 +20,7 @@ int main(int argc, FAR char *argv[])
     struct pwm_info_s info;
     int fd;
 
-    // PWM
-    memset(&info, 0, sizeof(struct pwm_info_s));
-    info.frequency = 1000;
-    info.duty = b16divi(uitoub16((uint8_t)10) - 1, 100);
-
-    fd = open("/dev/pwm1", O_RDONLY);
-    ioctl(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
-    ioctl(fd, PWMIOC_START, 0);
-    puts("Starting PWM pulse train");
-    sleep(10);
-    ioctl(fd, PWMIOC_STOP, 0);
-    puts("Stopping PWM pulse train");
-    close(fd);
-    fflush(stdout);
-
-    // GPIO
+    // GPIO 0
     bool outvalue = true;
     bool invalue;
     fd = open("/dev/gpio0", O_RDWR);
@@ -45,15 +30,46 @@ int main(int argc, FAR char *argv[])
     puts("Writing 1 to /dev/gpio0");
     ioctl(fd, GPIOC_READ, (unsigned long)((uintptr_t)&invalue));
     printf("Verify: Value=%u\n", (unsigned int)invalue);
+    close(fd);
 
-    sleep(10);
-
+    // GPIO 1
     outvalue = false;
+    fd = open("/dev/gpio1", O_RDWR);
+    ioctl(fd, GPIOC_SETPINTYPE, (unsigned long) GPIO_OUTPUT_PIN);
+
+    ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
+    puts("Writing 0 to /dev/gpio1");
+    ioctl(fd, GPIOC_READ, (unsigned long)((uintptr_t)&invalue));
+    printf("Verify: Value=%u\n", (unsigned int)invalue);
+    close(fd);
+    fflush(stdout);
+
+    // PWM 0
+    memset(&info, 0, sizeof(struct pwm_info_s));
+    info.frequency = 1000;
+    info.duty = b16divi(uitoub16((uint8_t)20) - 1, 100);
+
+    fd = open("/dev/pwm0", O_RDONLY);
+    ioctl(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
+    ioctl(fd, PWMIOC_START, 0);
+    puts("Starting PWM pulse train");
+    sleep(10);
+    ioctl(fd, PWMIOC_STOP, 0);
+    puts("Stopping PWM pulse train");
+    close(fd);
+    fflush(stdout);
+
+    // GPIO 0 - Low
+    outvalue = false;
+    fd = open("/dev/gpio0", O_RDWR);
+    ioctl(fd, GPIOC_SETPINTYPE, (unsigned long) GPIO_OUTPUT_PIN);
+
     ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
     puts("Writing 0 to /dev/gpio0");
     ioctl(fd, GPIOC_READ, (unsigned long)((uintptr_t)&invalue));
     printf("Verify: Value=%u\n", (unsigned int)invalue);
     close(fd);
     fflush(stdout);
+
     return OK;
 }
