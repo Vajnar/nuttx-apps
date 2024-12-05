2925998593e69c173e99d57b8d84751ee41ed025
Martin Vajnar <martin.vajnar@gmail.com>
2024-12-05 15:01:54 +0100

    TCP socket for controlling speed

--- apo/motor_pwm.c
+++ apo/motor_pwm.c
@@ -1,28 +1,24 @@
-#include <nuttx/config.h>
-
-#include <sys/types.h>
-#include <sys/ioctl.h>
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <debug.h>
-#include <string.h>
-#include <inttypes.h>
-#include <math.h>
 
 #include <nuttx/timers/pwm.h>
 #include <nuttx/ioexpander/gpio.h>
 #include <nuttx/sensors/qencoder.h>
 
-int main(int argc, FAR char *argv[])
+#include <sys/ioctl.h>
+
+int main(int argc, char *argv[])
 {
     struct pwm_info_s info;
     int fd;
-
-    // GPIO 0
+        // GPIO 0
     bool outvalue = false;
     bool invalue;
     fd = open("/dev/gpio0", O_RDWR);
@@ -82,6 +78,24 @@ int main(int argc, FAR char *argv[])
     ioctl(fd_qe1, QEIOC_POSITION, &pos);
     printf("QE1 Position=%ld\n", pos);
 
+    const int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+    const int bool_true = 1;
+    setsockopt(tcp_socket, SOL_SOCKET, SO_REUSEADDR, &bool_true, sizeof(bool_true));
+
+    struct sockaddr_in saddr;
+    memset(&saddr, 0, sizeof(struct sockaddr_in));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(5000);
+
+    bind(tcp_socket, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+    listen(tcp_socket, 1);
+    int tcp_conn_socket = accept(tcp_socket, NULL, NULL);
+    setsockopt(tcp_conn_socket, SOL_SOCKET, SO_KEEPALIVE, &bool_true, sizeof(bool_true));
+    setsockopt(tcp_conn_socket, IPPROTO_TCP, TCP_NODELAY, &bool_true, sizeof(bool_true));
+    int flags = fcntl(tcp_conn_socket, F_GETFL, 0);
+    fcntl(tcp_conn_socket, F_SETFL, flags | O_NONBLOCK);
+
     // PWM 1
     memset(&info, 0, sizeof(struct pwm_info_s));
     info.frequency = 8000;
@@ -95,59 +109,45 @@ int main(int argc, FAR char *argv[])
     int fd_pwm1 = open("/dev/pwm1", O_RDONLY);
     ioctl(fd_pwm1, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
     ioctl(fd_pwm1, PWMIOC_START, 0);
-    puts("Starting PWM pulse train");
-    int i = 1000;
-    // double int_val = 0;
-    while (i--) {
-        // ioctl(fd_qe0, QEIOC_POSITION, &pos);
-        // printf("QE0 Position=%ld, pass=%d\n", pos, i);
-        ioctl(fd_qe1, QEIOC_POSITION, &pos);
-        ioctl(fd_qe1, QEIOC_RESET, 0);
-        // double err = 60 - pos;
-        // int_val += err;
-        // double p = 5000 * err;
-        // double it = 100 * int_val;
-        // double output = p + it;
-        // if (output < 0) {
-        //     outvalue = false;
-        //     fd = open("/dev/gpio2", O_RDWR);
-        //     ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
-        //     close(fd);
-        //
-        //     outvalue = true;
-        //     fd = open("/dev/gpio3", O_RDWR);
-        //     ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
-        //     close(fd);
-        // } else {
-        //     outvalue = true;
-        //     fd = open("/dev/gpio2", O_RDWR);
-        //     ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
-        //     close(fd);
-        //
-        //     outvalue = false;
-        //     fd = open("/dev/gpio3", O_RDWR);
-        //     ioctl(fd, GPIOC_WRITE, (unsigned long)outvalue);
-        //     close(fd);
-        // }
-        // output = fabs(output);
-        // if (output < 13107) {
-        //     output = 13107;
-        // } else if (output > 15000) {
-        //     output = 15000;
-        // }
-        // info.duty = (uint16_t)output;
-        // ioctl(fd_pwm1, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
-        // printf("QE1 Position=%ld, info.duty=%lu, err=%lf, p=%lf, i=%lf\n", pos, info.duty, err, p, it);
-        printf("QE1 Position=%ld, remaining=%d\n", pos, i);
-        usleep(1000000);
+
+    while(1) {
+        char incoming[64];
+        int num_read = read(tcp_conn_socket, &incoming, sizeof(incoming));
+        if (num_read < 0 && errno == EWOULDBLOCK) {
+            sleep(1);
+            continue;
+        } else if (num_read <= 0) {
+            break;
+        }
+        //write(tcp_conn_socket, &incoming, num_read);
+        incoming[num_read-1] = '\0';
+        int pwm_left, pwm_right;
+        sscanf(incoming, "%d,%d", &pwm_left, &pwm_right);
+
+        printf("incoming: %s, pwm_left=%d, pwm_right=%d\n", incoming, pwm_left, pwm_right);
+        fflush(stdout);
+        if (pwm_left < 0) {
+            pwm_left = 0;
+        } else if (pwm_left > 50) {
+            pwm_left = 50;
+        }
+        if (pwm_right < 0) {
+            pwm_right = 0;
+        } else if (pwm_right > 50) {
+            pwm_right = 50;
+        }
+        info.duty = b16divi(uitoub16((uint8_t)pwm_left) - 1, 100);
+        ioctl(fd_pwm1, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
     }
+    close(tcp_conn_socket);
+    close(tcp_socket);
+
     // ioctl(fd_pwm0, PWMIOC_STOP, 0);
     ioctl(fd_pwm1, PWMIOC_STOP, 0);
     puts("Stopping PWM pulse train");
     // close(fd_pwm0);
     close(fd_pwm1);
     fflush(stdout);
-    sleep(1);
 
     // GPIO 0
     outvalue = false;
@@ -197,6 +197,5 @@ int main(int argc, FAR char *argv[])
 
     close(fd_qe0);
     close(fd_qe1);
-
-    return OK;
+    return 0;
 }
