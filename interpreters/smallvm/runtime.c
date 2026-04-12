5a29c45727cd06b6a8359a4495d17b934ebbce2f
Martin Vajnar <martin.vajnar@gmail.com>
2026-04-12 10:32:55 +0100

    smallVM: use ppoll() to wait for events

--- interpreters/smallvm/runtime.c
+++ interpreters/smallvm/runtime.c
@@ -625,6 +625,8 @@ static void sendMessage(int msgType, int chunkIndex, int dataSize, char *data) {
 
 int hasOutputSpace(int byteCount) { return ((OUTBUF_MASK - OUTBUF_BYTES()) > byteCount); }
 
+int bytesToOutput(void) { return OUTBUF_BYTES(); }
+
 static void waitForOutbufBytes(int bytesNeeded) {
 	// Wait until there is room for the given number of bytes in the output buffer.
 
