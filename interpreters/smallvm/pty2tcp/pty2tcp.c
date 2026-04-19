dad57f5e34b38fcd2de08ae67245fefa1e5ad578
Martin Vajnar <martin.vajnar@gmail.com>
2026-04-19 20:37:52 +0200

    smallvm: pty2tcp: rewrite using ppoll()

--- interpreters/smallvm/pty2tcp/pty2tcp.c
+++ interpreters/smallvm/pty2tcp/pty2tcp.c
@@ -9,6 +9,7 @@
 
 #define _XOPEN_SOURCE 600
 #define _DEFAULT_SOURCE
+#define _GNU_SOURCE
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -26,6 +27,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <poll.h>
 
 static int pty;
 static int tcp_socket;
@@ -62,31 +64,78 @@ static void openPseudoTerminal() {
 	makePtyFile();
 }
 
-void transferData(int from_fd, int to_fd) {
-	char buf[1500];
+void transferData() {
+	const int nfds = 2;
+	char buf[2][1024];
+	int recv[2] = {0,0};
+	int sent[2] = {0,0};
+	struct pollfd fds[2];
+	struct timespec timeout;
+	timeout.tv_sec = 30;
+	timeout.tv_nsec = 0;
 
-	int received = read(from_fd, buf, sizeof(buf));
-	if (received > 0) {
-		int sent = 0;
-		while(sent < received) {
-			int written = write(to_fd, &buf[sent], received - sent);
-			if (written < 0) written = 0;
-			sent += written;
+	memset(fds, 0, sizeof(fds));
+	for (int i = 0; i < 2; i++) {
+		fds[i].fd = (i == 0) ? pty : tcp_socket;
+		fds[i].events = POLLIN;
+	}
+
+	int ret = ppoll(fds, nfds, &timeout, NULL);
+	if (ret > 0) {
+		for (int i = 0; i < 2; i++) {
+			if (fds[i].revents & POLLIN) {
+				int readd = read(fds[i].fd, buf[i], sizeof(buf[0]));
+				if (readd < 0) {
+					perror("read()");
+				} else if (readd) {
+					recv[i] = readd;
+					printf("%d = read(%d, %p, %ld)\n", readd, fds[i].fd, buf[i], sizeof(buf[0]));
+				}
+			}
+		}
+	}
+
+	while (recv[0] > 0 || recv[1] > 0) {
+		timeout.tv_sec = 0;
+		timeout.tv_nsec = 0;
+		memset(fds, 0, sizeof(fds));
+		for (int i = 0; i < 2; i++) {
+			if (recv[i] > 0) {
+				fds[i].fd = (i == 0) ? tcp_socket : pty;
+			} else {
+				fds[i].fd = -1;
+			}
+			fds[i].events = POLLOUT;
+		}
+
+		ret = ppoll(fds, nfds, &timeout, NULL);
+		if (ret > 0) {
+			for (int i = 0; i < 2; i++) {
+				if (fds[i].revents & POLLOUT) {
+					int written = write(fds[i].fd, &buf[i][sent[i]], recv[i]);
+					printf("%d = write(%d, %p, %d)\n\n", written, fds[i].fd, &buf[i][sent[i]], recv[i]);
+					if (written < 0) {
+						written = 0;
+						perror("written(): ");
+					}
+					sent[i] += written;
+					recv[i] -= written;
+				}
+			}
 		}
 	}
 }
+
 void dataLoop() {
 	printf("TCP socket: %d\r\nPTY fd: %d\r\n", tcp_socket, pty);
 	while(1) {
-		transferData(pty, tcp_socket);
-		transferData(tcp_socket, pty);
-		usleep(10000);
+		transferData();
 	}
 }
 
 void setupTcpConnection(void) {
 	tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
-	const int bool_true = 1;
+//	const int bool_true = 1;
 
 	struct sockaddr_in saddr;
 	memset(&saddr, 0, sizeof(struct sockaddr_in));
@@ -100,7 +149,7 @@ void setupTcpConnection(void) {
 		exit(-1);
 	}
 //	ret = setsockopt(tcp_socket, SOL_SOCKET, SO_KEEPALIVE, &bool_true, sizeof(bool_true));
-	ret = setsockopt(tcp_socket, IPPROTO_TCP, TCP_NODELAY, &bool_true, sizeof(bool_true));
+//	ret = setsockopt(tcp_socket, IPPROTO_TCP, TCP_NODELAY, &bool_true, sizeof(bool_true));
 	int flags = fcntl(tcp_socket, F_GETFL, 0);
 	ret = fcntl(tcp_socket, F_SETFL, flags | O_NONBLOCK);
 }
